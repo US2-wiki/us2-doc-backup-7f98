87db5676f4769ef7a259f3fe06b8fb34fd54fd9c
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2023-11-27 15:23:14 +0000

    Move location

---
+++ page finder.py
@@ -0,0 +1,18 @@
+import os, pathlib
+with open(pathlib.Path(os.getcwd())/'wiki'/'Full index.md', 'r') as rfp:
+    content = rfp.read()
+    index = 0
+    while True:
+        location = content.find('[[', index)
+        if location == -1:
+            break
+        if content[location-1] == '!':
+            attachment = True
+        else:
+            attachment = False
+        end_bracket = content.find(']]', location)
+        if not attachment:
+            print(content[location+2:end_bracket])
+        index = end_bracket
+input()
+        
